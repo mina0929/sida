39f221ff85cf9f26187867fc3fb3dd45658da395
今井美奈 <106499852+mina0929@users.noreply.github.com>
2025-08-27 18:04:09 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import "./style.scss";
 import { initHeader } from "./js/header.js";
-import { initSalonsSwiper } from "./js/salon.js";
-import { initCompanySlider } from "./js/company.js";
-import { initTextMarquee } from "./js/recruit.js";
+//import { initSalonsSwiper } from "./js/salon.js";
+//import { initCompanySlider } from "./js/company.js";
+//import { initTextMarquee } from "./js/recruit.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHeader();
-  initSalonsSwiper();
-  initCompanySlider();
-  initTextMarquee();
+  //initSalonsSwiper();
+  //initCompanySlider();
+  //initTextMarquee();
 });
